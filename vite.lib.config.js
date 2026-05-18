d00dd38d5b786449f4604e524e021b3ba2d88a6f
Laurent Dethoor <ld2studiogame@gmail.com>
2026-05-18 15:20:02 +0200

    Initial commit

---
+++ vite.lib.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'node:path'
+
+export default {
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'lib/main.js'),
+            name: 'VirtualBuilder',
+            formats: ["es"],
+            fileName: () => 'index.js',
+        },
+        rollupOptions: {
+            // Mark three.js and all its submodules as external (peer dependency)
+            external: ['three', /^three\/.*/]
+        }
+    }
+}
