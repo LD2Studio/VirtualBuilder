0b5f591ca8067dfedd7e97df53a5fc01905ec9fb
Laurent Dethoor <ld2studiogame@gmail.com>
2026-05-18 19:06:56 +0200

    Add Input manager

---
+++ examples/helloworld/main.js
@@ -0,0 +1,42 @@
+import { app } from '../../lib/main.js'
+app.init({
+    interactive: true,
+    renderOptions: {
+        antialias: true,
+    }
+})
+
+const cubeDesc = {
+    name: 'My Cube',
+    rigid_bodies: [
+        {   id: 'rb1',
+            type: 'static',
+            meshes: [
+                {
+                    geometry: {
+                        shape: 'box',
+                        size: [1, 1, 1]
+                    },
+                    visual: {
+                        color: 'limegreen',
+                    },
+                    collider : true,
+                }
+            ],
+            // position: [0, 0.5, 0],
+            // rotation: [0, 0, 0],
+        }
+    ]
+}
+app.createAsset(cubeDesc)
+
+const keyA = app.addInput(['KeyQ'])
+keyA.on(
+    () => { console.log('KeyA is pressed')},
+    () => { console.log('KeyA is released')}
+)
+// console.log(keyA)
+
+app.onRender = () => {
+    // console.log(keyA.pressed)
+}
